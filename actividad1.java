901e63bbf6c314e05203199861b0da5f86cd7b19
AaronSantelices <aaronignacio71@gmail.com>
2025-10-07 19:55:22 +0200

    Add files via upload

---
+++ actividad1.java
@@ -0,0 +1,34 @@
+package com;
+import java.util.Scanner;
+public class actividad1 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.println("Dime un numero:");
+		num = sc.nextInt();
+	
+		while (num != 0){
+			int res = num * num;
+			if (num % 2 == 1) {
+				if (num < 0) {System.out.println("Tu número es impar, negativo y al cuadrado sería: " + res);}
+				if (num > 0) {System.out.println("Tu número es impar, positivo y al cuadrado sería: " + res);}
+			};
+			if (num % 2 == 0) {
+				if (num < 0) {System.out.println("Tu número es par, negativo y al cuadrado sería: " + res);}
+				if (num > 0) {System.out.println("Tu número es par, positivo y al cuadrado sería: " + res);}
+			};
+		System.out.println("Dime otro numero:");
+		num = sc.nextInt();
+		}
+		
+		
+		while (num == 0){
+			System.out.println("ERROR, NÚMERO SIN VALOR");
+			System.out.println("Dime un numero válido:");
+			num = sc.nextInt();
+			}
+		
+			
+		
+	}
+}
